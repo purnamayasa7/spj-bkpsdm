005ee5688cf109b933bae482de89f514f5e2a5df
DeX PY <purnamayasa0@gmail.com>
2025-12-16 10:44:23 +0800

    Updated 12-16-2025 3

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,12 +40,11 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('uploads'),
-            'url' => env('APP_URL') . '/uploads',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
         ],
+
 
         's3' => [
             'driver' => 's3',
