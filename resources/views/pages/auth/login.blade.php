22b30d5d4520db6769c45e65cbc7d92ae1ad0b63
DeX PY <purnamayasa0@gmail.com>
2025-12-19 13:27:45 +0800

    Updated login.blade.php

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="author" content="">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<meta name="description" content="This is a login page template based on Bootstrap 5">
+	<meta name="description" content="">
 	<title>E-SPJ - Login</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
